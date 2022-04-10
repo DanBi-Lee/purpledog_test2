8225cf5868d4cfde278c74d60db57175c493f182
DanBi-Lee <danbi.s.rain@gmail.com>
2022-04-10 21:02:02 +0900

    [#2] 라우터 적용 resolves #2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import CategoryListPage from "./pages/CategoryListPage";
-import PostListPage from "./pages/PostListPage";
 import PostDetailPage from "./pages/PostDetailPage";
+import PostListPage from "./pages/PostListPage";
 import "./style/reset.css";
 
 function App() {
   return (
-    <>
-      <CategoryListPage />
-      <PostListPage />
-      <PostDetailPage />
-    </>
+    <Routes>
+      <Route path="/" element={<CategoryListPage />} exact={true} />
+      <Route path="/category/:categoryId" element={<PostListPage />} />
+      <Route path="/post/:postId" element={<PostDetailPage />} />
+    </Routes>
   );
 }
 
