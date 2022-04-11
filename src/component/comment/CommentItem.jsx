ec4a91804e356ac5906d262e51e645678f563e1a
DanBi-Lee <danbi.s.rain@gmail.com>
2022-04-11 22:59:20 +0900

    [#9] 포스트 상세 페이지 댓글 불러오기
    resolves #9

--- src/component/comment/CommentItem.jsx
+++ src/component/comment/CommentItem.jsx
@@ -1,28 +1,42 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import ReCommentList from './ReCommentList';
 import styles from './CommentItem.module.css';
 import {ReactComponent as IconComment} from '../../assets/icon_comment.svg';
+import useHTTP from '../../hooks/useHTTP';
+import { getPost } from '../../api/post';
+import useObserverLazyLoad from '../../hooks/useObserverLazyLoad';
 
 function CommentItem({id}) {
     const [commentOpen, SetCommentOpen] = useState(false);
+    const $comment = useRef();
+    const {state: commentState, fetchData: fetchComment} = useHTTP(getPost);
+
+    useObserverLazyLoad($comment, fetchComment);
 
     const onCommentToggle = () => {
         SetCommentOpen(state=>!state);
     }
 
+    let author, date, text;
+    if(commentState.data){
+        author = commentState.data.by ;
+        date = commentState.data.time ;
+        text = commentState.data.text;
+    }
+
+    console.log(commentState);
+
     return (
-        <article className={styles.comment} data-id={id}>
+        <article className={styles.comment} data-id={id} ref={$comment}>
             <header className={styles.comment__header}>
                 <p className={styles.comment_author}>
-                    작성자
+                    {author}
                 </p>
                 <p className={styles.comment_date}>
-                    날짜
+                    {date}
                 </p>
             </header>
-            <div className={styles.comment_content}>
-                덧글 내용
-            </div>
+            <div className={styles.comment_content} dangerouslySetInnerHTML={{__html: text}}  />
             <footer className={styles.comment__footer}>
                 <div className={styles.button__wrap}>
                     <button  className={`${styles.comment_button} ${commentOpen? styles.on : ''}`} onClick={onCommentToggle}>
